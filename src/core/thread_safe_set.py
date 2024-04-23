0f56d85648cbe70eaccf363f0b204bda4b4c70b3
amitp970 <amitperi56@gmail.com>
2024-04-23 22:53:55 +0300

    Add thread_safe_set

---
+++ src/core/thread_safe_set.py
@@ -0,0 +1,23 @@
+import threading
+
+class ThreadSafeSet:
+    def __init__(self):
+        self.set = set()
+        self.lock = threading.Lock()
+    
+    def add(self, item):
+        with self.lock:
+            self.set.add(item)
+    
+    def remove(self, item):
+        with self.lock:
+            self.set.remove(item)
+    
+    def contains(self, item):
+        return item in self.set
+    
+    def get_all(self):
+        return self.set.copy()
+    
+    def __contains__(self, item):
+        return self.contains(item)
